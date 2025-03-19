4e3b0e0f30d06ce59af2a9d1b5695472e1b92f72
Suresh kumara <kumara.suresh25@gmail.com>
2025-03-19 22:02:49 +0530

    Update run-on-commit-history.sh

--- scripts/run-on-commit-history.sh
+++ scripts/run-on-commit-history.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Forward the arguments received by the shell script to the Python script
-python3 ./scripts/run-on-commit-history.py "$@"
+python3 ./scripts/run_on_commit_history.py "$@"
